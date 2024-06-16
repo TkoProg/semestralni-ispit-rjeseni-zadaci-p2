14f2fdd1ef4b249bd13fdc217bb195a01b99eeb7
tamiroladejo <koltam09@gmail.com>
2024-06-17 01:30:54 +0200

    Grupa 2 Zadatak 3 uradjen.

--- Grupa 2/Zadaci/Zadatak 3.cpp
+++ Grupa 2/Zadaci/Zadatak 3.cpp
@@ -1,3 +1,49 @@
-//
-// Created by Tamir Oladejo on 16. 6. 2024..
-//
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+// Ponovo Adisov zadatak, stvarno nemam kuveta da pokusavam uciti ovo sad lol
+
+template<typename tip>
+bool nalazi_se(const set<tip>& s, tip a) {
+    for (auto it(s.begin()); it != s.end(); it++)
+        if (*it == a)
+            return true;
+    return false;
+}
+
+template<typename tip>
+set<tip> f(const set<tip>& s1, const set<tip>& s2) {
+    set<tip> razlika;
+    for (auto it(s1.begin()); it != s1.end(); it++)
+        if (!nalazi_se(s2, *it))
+            razlika.insert(*it);
+    for (auto it(s2.begin()); it != s2.end(); it++)
+        if (!nalazi_se(s1, *it))
+            razlika.insert(*it);
+    return razlika;
+}
+
+int main() {
+    int na(0);
+    std::cin >> na;
+    set<double> A;
+    for (int i(0); i < na; i++) {
+        double x;
+        std::cin >> x;
+        A.insert(x);
+    }
+    int nb(0);
+    std::cin >> nb;
+    set<double> B;
+    for (int i(0); i < nb; i++) {
+        double x;
+        std::cin >> x;
+        B.insert(x);
+    }
+    set<double> C = f(A, B);
+    for (auto it(C.begin()); it != C.end(); it++)
+        cout << *it << " ";
+    return 0;
+}
