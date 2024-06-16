ceb3456b83631ab44c93aa8a57a14e5ebecdc57c
tamiroladejo <koltam09@gmail.com>
2024-06-17 00:55:18 +0200

    Grupa 2 Zadatak 1 uradjen.

--- Grupa 2/Zadaci/Zadatak 1.cpp
+++ Grupa 2/Zadaci/Zadatak 1.cpp
@@ -1,3 +1,36 @@
-//
-// Created by Tamir Oladejo on 16. 6. 2024..
-//
+#include <iostream>
+#include <vector>
+#include <exception>
+
+using namespace std;
+
+// Poprilicno EREGANTOOO rijesenje sta ja znam, it works lmao
+
+double F(vector<double> &v) {
+    double rezultat = 0, broj = 0;
+    for(int i = v.size() - 1; i >= 0; i--) {
+        if((v[i] + broj) < 0) {
+            throw domain_error("Podkorijena velicina je negativna.");
+        }
+        rezultat = sqrt(v[i] + broj);
+        broj = rezultat;
+    }
+    return rezultat;
+}
+
+int main() {
+    try {
+        // int n;
+        // cin>>n;
+        vector<double> v{1, 4, 9, -90, 3, 77};
+        //for(int i = 0; i < n; i++) {
+        //    cin>>v[i];
+        //}
+        cout.precision(5);
+        cout<<F(v);
+        return 0;
+    }
+    catch(domain_error &poruka) {
+        cout<<poruka.what();
+    }
+}
