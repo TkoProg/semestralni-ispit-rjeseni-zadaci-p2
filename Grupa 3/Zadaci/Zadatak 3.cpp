9b61d8eaf2382ebf7dc1ac284bdca607733c707d
tamiroladejo <koltam09@gmail.com>
2024-06-17 02:59:58 +0200

    Grupa 3 Zadatak 3 uradjen.

--- Grupa 3/Zadaci/Zadatak 3.cpp
+++ Grupa 3/Zadaci/Zadatak 3.cpp
@@ -1,3 +1,28 @@
-//
-// Created by Tamir Oladejo on 16. 6. 2024..
-//
+#include <iostream>
+#include <functional>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+// Jos jedan od adisovih zadataka, u gasu smo... (I AINT DOING THIS DAWG)
+
+function<double(double)> f(function<double(double)> f1, function<double(double)> f2) {
+    return [f1, f2](double x) {
+    return f2(f1(x));
+    };
+}
+
+double f1(double x) {
+    return sin(x);
+}
+
+int main() {
+    double x;
+    cin >> x;
+    auto g(f(f1, [](double x) {
+        return 5 * x * x * x * x - 2 * x * x * x + x * x - 1;
+    }));
+    cout << setprecision(5) << g(x);
+    return 0;
+}
