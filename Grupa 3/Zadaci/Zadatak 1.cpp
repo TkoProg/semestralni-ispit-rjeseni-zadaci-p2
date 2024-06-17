d3b8020ad5cce04afc647561c65289ddaf682b63
tamiroladejo <koltam09@gmail.com>
2024-06-17 02:00:13 +0200

    Grupa 3 Zadatak 1 uradjen.

--- Grupa 3/Zadaci/Zadatak 1.cpp
+++ Grupa 3/Zadaci/Zadatak 1.cpp
@@ -1,3 +1,30 @@
-//
-// Created by Tamir Oladejo on 16. 6. 2024..
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Poprilicno EREGANTOOOOO rjesenje, svidja mi se
+
+double F(vector<double> &v) {
+    double suma = 0;
+    for(int i = 1; i < v.size() + 1; i++) {
+        double nazivnik = 0;
+        for(int j = 0; j < i; j++) {
+            nazivnik += v[j];
+            if(nazivnik == 0) throw domain_error("Dijeljenje sa nulom nije definisano.");
+        }
+        suma += 1/nazivnik;
+    }
+    return suma;
+}
+
+int main() {
+    try {
+        vector<double> v={1,-1,40,10,2};
+        cout.precision(5);
+        cout<<F(v);
+    }
+    catch(domain_error &poruka) {
+        cout<<poruka.what();
+    }
+}
